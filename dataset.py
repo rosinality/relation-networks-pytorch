03b4bcc408fdd8bdb51fe2d1c8bf566cbf404742
Kim Seonghyeon <kim.seonghyeon@snu.ac.kr>
2017-06-08 21:07:36 +0900

    Fixed bug

--- dataset.py
+++ dataset.py
@@ -3,12 +3,13 @@ import pickle
 
 import numpy as np
 from PIL import Image
+import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
 
 class CLEVR(Dataset):
     def __init__(self, root, split='train', transform=None):
-        with open(f'data/{split}.pkl', 'wb') as f:
+        with open(f'data/{split}.pkl', 'rb') as f:
             self.data = pickle.load(f)
 
         self.data
@@ -18,9 +19,8 @@ class CLEVR(Dataset):
 
     def __getitem__(self, index):
         imgfile, question, answer = self.data[index]
-        with open(os.path.join(self.root, images, self.mode, imgfile)) as f:
-            img = Image.open(f)
-
+        img = Image.open(os.path.join(self.root, 'images', self.split, imgfile))
+        
         img = self.transform(img)
 
         return img, question, answer
@@ -29,7 +29,7 @@ class CLEVR(Dataset):
         return len(self.data)
 
 transform = transforms.Compose([
-    transforms.Scale([128, 128]),
+    transforms.Scale(128),
     transforms.Pad(4),
     transforms.RandomCrop([128, 128]),
     transforms.ToTensor(),
@@ -40,11 +40,13 @@ transform = transforms.Compose([
 def collate_data(batch):
     images, answers = [], []
     batch_size = len(batch)
-    max_len = len(batch[0][0])
+
+    max_len = max(map(lambda x: len(x[1]), batch))
+
     questions = np.zeros((batch_size, max_len), dtype=np.int64)
     sort_by_len = sorted(batch, key=lambda x: len(x[1]), reverse=True)
 
-    for i, b in enumerate(batch):
+    for i, b in enumerate(sort_by_len):
         image, question, answer = b
         images.append(image)
         length = len(question)
